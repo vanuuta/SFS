c1b1b45252e3d2d532a5f60658189a43aadf2657
Ivan Petrov <vanishivanish26@gmail.com>
2025-05-19 23:17:44 +0300

    fixes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include "mainwindow.h"
+// #include "ui/mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    // ApplicationManager manager;
+    // manager.launch();
+    return app.exec();
 }
+
